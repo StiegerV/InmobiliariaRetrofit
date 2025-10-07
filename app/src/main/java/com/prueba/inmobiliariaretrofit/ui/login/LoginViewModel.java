cdea7356a09e39ce469808ee6bcde30d19be7c68
Valentin Stieger <50178370+StiegerV@users.noreply.github.com>
2025-10-07 09:57:09 -0300

    ni me acuerdo

--- app/src/main/java/com/prueba/inmobiliariaretrofit/ui/login/LoginViewModel.java
+++ app/src/main/java/com/prueba/inmobiliariaretrofit/ui/login/LoginViewModel.java
@@ -2,6 +2,7 @@ package com.prueba.inmobiliariaretrofit.ui.login;
 
 import android.app.Application;
 import android.content.Intent;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -9,8 +10,12 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.prueba.inmobiliariaretrofit.MainActivity;
+import com.prueba.inmobiliariaretrofit.request.ApiClient;
 
-import Modelos.UserModel;
+import modelos.UserModel;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginViewModel extends AndroidViewModel {
 
@@ -48,6 +53,35 @@ public class LoginViewModel extends AndroidViewModel {
         }
 
 
+       ApiClient.InmoService llamadas= ApiClient.getInmoService();
+
+      Call<String> respuesta=llamadas.loginForm(email,password);
+      respuesta.enqueue(new Callback<String>() {
+          @Override
+          public void onResponse(Call<String> call, Response<String> response) {
+              if (response.isSuccessful()){
+                    String token=response.body();
+
+                  ApiClient.guardarToken(getApplication(),token);
+
+                  Intent intent = new Intent(getApplication(), MainActivity.class);
+
+                  intent.putExtra("User", "KUS");
+
+                  intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+
+                  getApplication().startActivity(intent);
+
+              }else{
+                  Log.d("token",response.code()+"");
+              }
+          }
+
+          @Override
+          public void onFailure(Call<String> call, Throwable throwable) {
+              Log.d("token",throwable.getMessage());
+          }
+      });
 
         if ( email.equals("tuqi") && password.equals("123")) {
 
---
+++ app/src/main/java/modelos/PropietarioModel.java
@@ -0,0 +1,12 @@
+package modelos;
+
+import java.io.Serializable;
+
+public class PropietarioModel implements Serializable {
+    private int idPropietario;
+
+    private String nombre,apellido,dni,telefono,email,clave;
+
+
+
+}
