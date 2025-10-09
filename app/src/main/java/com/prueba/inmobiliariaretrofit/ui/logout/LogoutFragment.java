6a70d512b025e4bb40caf2dc773783d945b88e22
Valentin Stieger <50178370+StiegerV@users.noreply.github.com>
2025-10-09 20:36:55 -0300

    sui

--- app/src/main/java/com/prueba/inmobiliariaretrofit/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/prueba/inmobiliariaretrofit/ui/logout/LogoutFragment.java
@@ -2,6 +2,9 @@ package com.prueba.inmobiliariaretrofit.ui.logout;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,9 +16,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.prueba.inmobiliariaretrofit.R;
+import com.prueba.inmobiliariaretrofit.databinding.FragmentLogoutBinding;
 
 public class LogoutFragment extends Fragment {
 
+    private FragmentLogoutBinding binding;
     private LogoutViewModel mViewModel;
 
     public static LogoutFragment newInstance() {
@@ -25,14 +30,29 @@ public class LogoutFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        mViewModel= new ViewModelProvider(this).get(LogoutViewModel.class);
+        binding=FragmentLogoutBinding.inflate(inflater,container,false);
+        muestraDialog(getActivity());
         return inflater.inflate(R.layout.fragment_logout, container, false);
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(LogoutViewModel.class);
-        // TODO: Use the ViewModel
+    private void muestraDialog(Context donde){
+        new AlertDialog.Builder(donde)
+                .setTitle("Salir")
+                .setMessage("Cerrar aplicacion?")
+                .setIcon(android.R.drawable.ic_lock_power_off)
+                .setPositiveButton(android.R.string.yes,new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface di,int i){
+                        getActivity().finish();
+                    }
+                })
+                .setNegativeButton(android.R.string.no,new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface di,int i){
+                        getActivity().getSupportFragmentManager().popBackStack();
+                    }
+                }).show();
     }
 
 }
